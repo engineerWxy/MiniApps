36e16e7815e6b1e3c36905e0ac6ef8228c112dcc
nick.wu1 <nick.wu@evg.co>
2024-08-22 15:24:19 +0800

    update ssl

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     https: {
-      key: '/Users/wuxingyu/react_work/MiniAPP/ssl/127.0.0.1-key.pem',
-      cert: '/Users/wuxingyu/react_work/MiniAPP/ssl/127.0.0.1.pem'
+      key: '../ssl/127.0.0.1-key.pem',
+      cert: '../ssl/127.0.0.1.pem'
     }
   }
 })
